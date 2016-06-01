a8fec8f092ebb410959df29a848349c166ad54e7
Žan Jonke <zj0527@student.uni-lj.si>
2016-06-01 12:05:10 +0000

    začetek dela

--- knjiznice/js/koda.js
+++ knjiznice/js/koda.js
@@ -40,3 +40,29 @@ function generirajPodatke(stPacienta) {
 
 
 // TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+
+var baseUrl = 'https://rest.ehrscape.com/rest/v1';
+var queryUrl = baseUrl + '/query';
+
+var username = "ois.seminar";
+var password = "ois4fri";
+
+
+/**
+ * Prijava v sistem z privzetim uporabnikom za predmet OIS in pridobitev
+ * enolične ID številke za dostop do funkcionalnosti
+ * @return enolični identifikator seje za dostop do funkcionalnosti
+ */
+function getSessionId() {
+    var response = $.ajax({
+        type: "POST",
+        url: baseUrl + "/session?username=" + encodeURIComponent(username) +
+                "&password=" + encodeURIComponent(password),
+        async: false
+    });
+    return response.responseJSON.sessionId;
+}
+
+
+
+
